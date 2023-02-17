7d9f7764a7de85b4c322b65820d05e55eaa67a57
Bruno Domingos Teixeira <bruninhodt2467@gmail.com>
2023-02-17 18:05:34 -0300

    Método POST implementado

--- src/main/java/com/brunidomin/workshopmongo/service/UserService.java
+++ src/main/java/com/brunidomin/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brunidomin.workshopmongo.domain.User;
+import com.brunidomin.workshopmongo.dto.UserDTO;
 import com.brunidomin.workshopmongo.repository.UserRepository;
 import com.brunidomin.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -26,4 +27,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromUser(UserDTO objDTO) {
+		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
+	}
 }
