b790dcb4f5faf3e2f93201a21ad98d15d56a9d6f
Bruno Domingos Teixeira <bruninhodt2467@gmail.com>
2023-02-22 20:42:39 -0300

    Consulta simples com @Query

--- src/main/java/com/brunidomin/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/brunidomin/workshopmongo/repository/PostRepository.java
@@ -3,13 +3,20 @@ package com.brunidomin.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.brunidomin.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+//                                 ||
+	@Query("{ 'title': { $regex : ?0, $options : 'i' } }")
+//                                 ||
+//								   \/	
+	List<Post> searchTitle(String text);
+	
+	
 	//Método de Query 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }			 //------title	
